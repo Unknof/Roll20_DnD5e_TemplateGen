4ae0c7d9b2654aa72396390c3e3e2c69f4e5238a
Skoda <philipp.skoda@siemens.com>
2020-06-11 13:53:26 +0200

    crit bug fixed

--- tmpFiller.py
+++ tmpFiller.py
@@ -7,17 +7,15 @@ def home():
 
 @get('/create')
 def create():
-    tmp ="&{template:atkdmg} {{rname=mname}} {{r1=[[d20attmcs>critc]]}} {{normal=1}} {{attack=1}} {{damage=1}} {{dmg1flag=1}} {{dmg1=[[dmgr]]}} {{dmg1type=dmgt}} {{crit1=[[critdmg]]}} {{desc=descr}}"
+    tmp ="&{template:atkdmg} {{rname=mname}} {{r1=[[d20cs>critcattm]]}} {{normal=1}} {{attack=1}} {{damage=1}} {{dmg1flag=1}} {{dmg1=[[dmgr]]}} {{dmg1type=dmgt}} {{crit1=[[critdmg]]}} {{desc=descr}}"
     mname = request.GET.get("mname")
     tmp =tmp.replace("mname", mname)
     descr = request.GET.get("descr")
     tmp =tmp.replace("descr", descr)
     attm = request.GET.get("attm")
     tmp =tmp.replace("attm", "+" + attm)
-
     dmgr = request.GET.get("dmgr")
     tmp =tmp.replace("dmgr", dmgr)
-
     dmgt = request.GET.get("dmgt")
     tmp =tmp.replace("dmgt", dmgt)
 
@@ -36,16 +34,4 @@ def create():
     return tmp
 
 
-@route('/<filepath>')
-def NewWay(filepath):
-    return template(filepath)
-
 run(host='localhost', port=8080)
-
-#mname
-#attr
-#dmg
-#dmgtype
-
-#critdmg
-#desc
